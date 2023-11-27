9858fc31d86e4d764fb4798c23c3e3c97a51a797
sunshinePlusIce <sunshineplusice@gmail.com>
2023-11-27 23:42:28 +0900

    setup basic framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import Example from './example';
+import './dist/output.css'
+import ValueInput from './ValutInput';
+
+function Canvas() {
+  return (
+    <>
+      <div className="userbox-canvas">
+
+      </div>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-screen flex flex-row w-full">
+        <div className="h-full w-2/3 bg-slate-300">
+          <div className="h-full flex flex-col">
+            <div className="bg-slate-400 basis-2/3"></div>
+            <div className="bg-slate-500"></div>
+          </div>
+        </div>
+        <div className="h-full w-1/3 bg-slate-600">
+          <div className="w-full h-1/6 flex flex-row">
+            <div className="h-full w-1/2 px-1">
+              <ValueInput 
+                leading={""} 
+                propName={"Width"} 
+                unitList={[
+                  "px"
+                ]}
+                placeholder={250}
+              />
+            </div>
+            <div className="h-full w-1/2 px-1">
+            <ValueInput 
+                leading={""} 
+                propName={"Height"} 
+                unitList={[
+                  "px"
+                ]}
+                placeholder={50}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
